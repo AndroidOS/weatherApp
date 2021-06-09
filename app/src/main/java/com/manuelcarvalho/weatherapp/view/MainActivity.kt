b14ef60dcb5eba24470c3092de0eba4981282954
AndroidOS <burrowsgit@gmail.com>
2021-06-09 19:28:55 +1000

    Added Internet permission

--- app/src/main/java/com/manuelcarvalho/weatherapp/view/MainActivity.kt
+++ app/src/main/java/com/manuelcarvalho/weatherapp/view/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.manuelcarvalho.weatherapp
+package com.manuelcarvalho.weatherapp.view
 
 import android.os.Bundle
 import com.google.android.material.snackbar.Snackbar
@@ -9,6 +9,7 @@ import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import android.view.Menu
 import android.view.MenuItem
+import com.manuelcarvalho.weatherapp.R
 import com.manuelcarvalho.weatherapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
