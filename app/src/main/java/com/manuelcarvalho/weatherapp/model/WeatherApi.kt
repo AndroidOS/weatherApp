e4e8ef505091558ccca8c108e56ec49139a1f894
AndroidOS <burrowsgit@gmail.com>
2021-06-21 19:23:01 +1000

    Added WeatherApiService.kt (1)

--- app/src/main/java/com/manuelcarvalho/weatherapp/model/WeatherApi.kt
+++ app/src/main/java/com/manuelcarvalho/weatherapp/model/WeatherApi.kt
@@ -16,11 +16,11 @@ interface WeatherApi {
 //    @GET("query?format=geojson&starttime=2019-12-11&endtime=2019-12-12")
 //    fun getQuakes(): Single<Root>
 
-//    @GET("data/2.5/weather?q=London,uk&APPID=${weatherKey}")
-//    fun getWeather(): Single<Root>
-
-    @GET("data/2.5/weather?q=London")
+    @GET("data/2.5/weather?q=London,uk&APPID=${weatherKey}")
     fun getWeather(): Single<Root>
+
+//    @GET("data/2.5/weather?q=London")
+//    fun getWeather(): Single<Root>
 
 //    @GET("query")
 //    fun getWeathers(@Query("format") format: String, @Query("starttime") startTime: String, @Query("endtime") endTime: String): Single<Root>
--- app/src/main/java/com/manuelcarvalho/weatherapp/model/WeatherApiService.kt
+++ app/src/main/java/com/manuelcarvalho/weatherapp/model/WeatherApiService.kt
@@ -7,7 +7,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 class WeatherApiService {
 
-    private val BASE_URL = "api.openweathermap.org/"
+    private val BASE_URL = "https://api.openweathermap.org/"
     //private val BASE_URL = "https://earthquake.usgs.gov/fdsnws/event/1/"
 
     private val api = Retrofit.Builder()
