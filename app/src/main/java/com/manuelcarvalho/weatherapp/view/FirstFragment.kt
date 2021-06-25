efe274032fd0f32bff103cd2ff5c848bf4fabe1c
AndroidOS <burrowsgit@gmail.com>
2021-06-26 05:53:56 +1000

    Added fragment viewmodel (2)

--- app/src/main/java/com/manuelcarvalho/weatherapp/view/FirstFragment.kt
+++ app/src/main/java/com/manuelcarvalho/weatherapp/view/FirstFragment.kt
@@ -1,19 +1,19 @@
 package com.manuelcarvalho.weatherapp.view
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import androidx.navigation.fragment.findNavController
 import com.manuelcarvalho.weatherapp.R
 import com.manuelcarvalho.weatherapp.databinding.FragmentFirstBinding
 import com.manuelcarvalho.weatherapp.viewmodel.ListViewModel
 
-/**
- * A simple [Fragment] subclass as the default destination in the navigation.
- */
+private const val TAG = "FirstFragment"
 class FirstFragment : Fragment() {
 
     private var _binding: FragmentFirstBinding? = null
@@ -43,10 +43,23 @@ class FirstFragment : Fragment() {
         binding.buttonFirst.setOnClickListener {
             findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
         }
+
+        observeViewModel()
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    fun observeViewModel() {
+
+        viewModel.weatherEvent.observe(this, Observer { loading ->
+            loading?.let {
+                Log.d(TAG,"weatherEvent changed")
+            }
+        })
+
+
     }
 }
