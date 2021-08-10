844dd0188543fe08ec2ae610df80566dee064b2f
AndroidOS <burrowsgit@gmail.com>
2021-08-10 17:44:50 +1000

    added Weather1.kt (2)

--- app/src/main/java/com/manuelcarvalho/weatherapp/model/Weather1.kt
+++ app/src/main/java/com/manuelcarvalho/weatherapp/model/Weather1.kt
@@ -6,7 +6,9 @@ import androidx.room.PrimaryKey
 @Entity
 data class Weather1(
     val place: String?,
-    val mag: Double?
+    val maxTemp: Double?,
+    val minTemp: Double?,
+    val barometer: Int
 ) {
     @PrimaryKey(autoGenerate = true)
     var uuid: Int = 0
